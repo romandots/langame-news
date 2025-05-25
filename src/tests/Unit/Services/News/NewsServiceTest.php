12c7284ace9f8c3669fc71c0f76b522c4ea7f6e2
romandots <ram.d.kreiz@gmail.com>
2025-05-25 15:21:02 +0300

    Update tests

--- src/tests/Unit/Services/News/NewsServiceTest.php
+++ src/tests/Unit/Services/News/NewsServiceTest.php
@@ -3,11 +3,13 @@
 namespace Tests\Unit\Services\News;
 
 use App\DTO\News;
+use App\Events\NewsAddedEvent;
 use App\Services\News\NewsService;
 use App\Services\News\Contracts\NewsAggregatorInterface;
 use App\Repositories\NewsRepository;
-use PHPUnit\Framework\TestCase;
+use Illuminate\Support\Facades\Event;
 use Psr\Log\LoggerInterface;
+use Tests\TestCase;
 
 class NewsServiceTest extends TestCase
 {
@@ -23,20 +25,42 @@ class NewsServiceTest extends TestCase
             ext_id: '12345',
         );
 
+        $newsRecord = new \App\Models\News([
+            'title' => $newsItem->title,
+            'summary' => $newsItem->summary,
+            'description' => $newsItem->description,
+            'url' => $newsItem->url,
+            'source' => $newsItem->source,
+            'published_at' => $newsItem->published_at,
+            'ext_id' => $newsItem->ext_id,
+        ]);
+
         $newsAggregator = $this->createMock(NewsAggregatorInterface::class);
         $newsAggregator->method('getSource')->willReturn('test_source');
         $newsAggregator->method('getNews')->willReturn([$newsItem]);
 
         $newsRepository = $this->createMock(NewsRepository::class);
         $newsRepository->method('getLastPublishedDateForSource')->willReturn(null);
-        $newsRepository->expects($this->once())->method('create')->with($newsItem);
+        $newsRepository->expects($this->once())->method('create')->with($newsItem)->willReturn($newsRecord);
 
         $logger = $this->createMock(LoggerInterface::class);
         $logger->expects($this->never())->method('error');
 
         $service = new NewsService($newsRepository, $logger);
 
+        Event::fake();
+
         $result = $service->aggregateNews($newsAggregator);
+
+        Event::assertDispatched(NewsAddedEvent::class, function ($event) use ($newsRecord, $newsItem) {
+            return $event->news->title === $newsRecord->title &&
+                   $event->news->summary === $newsRecord->summary &&
+                   $event->news->description === $newsRecord->description &&
+                   $event->news->url === $newsRecord->url &&
+                   $event->news->source === $newsRecord->source &&
+                   $event->news->published_at == $newsRecord->published_at &&
+                   $event->news->ext_id === $newsRecord->ext_id;
+        });
 
         $this->assertEquals(1, $result);
     }
