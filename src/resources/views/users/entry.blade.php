5b9f9ea1289beb51a202dd9022c50e960229c967
romandots <ram.d.kreiz@gmail.com>
2025-05-25 16:47:45 +0300

    Add light/dark theme support

--- src/resources/views/users/entry.blade.php
+++ src/resources/views/users/entry.blade.php
@@ -1,4 +1,4 @@
-<tr class="{{ !$user->is_confirmed ? 'bg-gray-200 dark:bg-gray-800 text-gray-500' : '' }}">
+<tr class="{{ !$user->is_confirmed ? 'bg-gray-200 dark:bg-gray-800 text-gray-500 dark:text-gray-400' : 'bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100' }}">
     <td class="px-4 py-2">{{ $user->id }}</td>
     <td class="px-4 py-2">{{ $user->name }}</td>
     <td class="px-4 py-2">{{ $user->email }}</td>
